32627b1771bcf1f0f8e334c5a81f42e90ef2c90e
Unknown <dlomugdang@mail.csuchico.edu>
2017-03-07 16:45:32 -0800

    check for good/bad username and password

--- form-validation.js
+++ form-validation.js
@@ -1,19 +1,30 @@
 function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
+  var userLength = userEntered.length;
+  if(userLength >= 6){
+    //Turn the username items green
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else{
+    //Show message that there is an error with the username...
+    //Turn the username items red
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
   var passEntered = document.getElementById("pass").value;
-
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
-
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if(passEntered == "password"){
+    //Show message that there is an error with the password...
+    //Turn the username items red
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else{
+  //Turn the username items green
+  document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
